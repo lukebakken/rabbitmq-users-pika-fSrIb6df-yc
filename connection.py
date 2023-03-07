79a7a28528e5688ae1ae46cf04701031147c2b45
Luke Bakken <luke@bakken.io>
2023-03-07 14:17:28 -0800

    Example is complete

--- connection.py
+++ connection.py
@@ -1,17 +1,32 @@
-def get_rabbit_connection():
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            heartbeat=0,
-            host=settings.RABBITMQ_HOST,
-            port=settings.RABBITMQ_PORT,
-            credentials=PlainCredentials(
-                settings.RABBITMQ_DEFAULT_USER, settings.RABBITMQ_DEFAULT_PASS
-            ),
-        )
+import functools
+import pika
+import threading
+
+rmq_conn = None
+rmq_ch = None
+
+
+def rabbitmq_thread(connected_event: threading.Event, stop_event: threading.Event):
+    global rmq_conn
+    global rmq_ch
+    c = pika.PlainCredentials("guest", "guest")
+    rmq_conn = pika.BlockingConnection(pika.ConnectionParameters(credentials=c))
+    rmq_ch = rmq_conn.channel()
+    rmq_ch.queue_declare(queue="my_queue")
+    connected_event.set()
+    while not stop_event.is_set():
+        rmq_conn.process_data_events(time_limit=1)
+
+
+def rabbitmq_publish(msg: str):
+    cb = functools.partial(do_publish, msg)
+    rmq_conn.add_callback_threadsafe(cb)
+
+
+def do_publish(msg: str):
+    p = pika.BasicProperties(
+        content_type="text/plain", delivery_mode=pika.DeliveryMode.Persistent
     )
-    return connection
-
-
-rabbitmq_client = get_rabbit_connection()
-rabbitmq_channel = rabbitmq_client.channel()
-rabbitmq_channel.queue_declare(queue="my_queue")
+    rmq_ch.basic_publish(
+        exchange="", routing_key="my_queue", body=msg, properties=p, mandatory=True
+    )
--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-﻿import pika
+﻿import logging
 import threading
-from connection import rabbit_channel
+from connection import rabbitmq_thread, rabbitmq_publish
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-def main(ch, msgs):
-    ch.basic_publish(
-        exchange="",
-        routing_key="my_queue",
-        body=msgs,
-        properties=pika.BasicProperties(delivery_mode=2),
-    )
+def publish_message(msg):
+    rabbitmq_publish(msg)
 
 
 if __name__ == "__main__":
-    thread = threading.Thread(target=main, args=(ch, msgs))
-    thread.start()
+    stop_event = threading.Event()
+    connected_event = threading.Event()
+
+    logger.info("starting rabbitmq_thread...")
+    rabbitmq_thread = threading.Thread(
+        target=rabbitmq_thread, args=(connected_event, stop_event)
+    )
+    rabbitmq_thread.start()
+    connected_event.wait()
+
+    logger.info("rabbitmq_thread started, publishing a message...")
+    publish_message("hello")
+
+    logger.info("stopping and waiting for rabbitmq_thread to exit...")
+    stop_event.set()
+    rabbitmq_thread.join()
+
+    logger.info("exiting")
