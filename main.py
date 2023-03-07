810ab75ea81d0a2145a200f0b5584252383d8108
Luke Bakken <luke@bakken.io>
2023-03-07 13:38:48 -0800

    fix syntax errors

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-﻿import threading
+﻿import pika
+import threading
 from connection import rabbit_channel
 
+
 def main(ch, msgs):
-         ch.basic_publish(
-         exchange="",
-         routing_key="my_queue",
-         body=msgs
-         properties=BasicProperties(
-             delivery_mode=2
-         )
-     )
-if __name__=="__main__":
-      thread = threading.Thread(target=main, args=(ch, msgs))
-      thread.start()
+    ch.basic_publish(
+        exchange="",
+        routing_key="my_queue",
+        body=msgs,
+        properties=pika.BasicProperties(delivery_mode=2),
+    )
+
+
+if __name__ == "__main__":
+    thread = threading.Thread(target=main, args=(ch, msgs))
+    thread.start()
